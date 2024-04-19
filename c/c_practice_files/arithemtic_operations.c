ba57a3e2635a6044a82c13161f819b45cd4527c5
Kipland Melton <kiplandmelton@gmail.com>
2024-04-19 11:04:44 -0500

    teaching myself C by redoing the problems i've already done in python

---
+++ c/c_practice_files/arithemtic_operations.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int a = 5, b = 3;
+	printf("Sum: %d\n",a+b);
+	printf("Difference: %d\n",a-b);
+	printf("Product: %d\n",a*b);
+	printf("Quotient: %d\n",a/b);
+	printf("Reminder: %d\n",a%b);
+	return 0;
+}
---
+++ c/c_practice_files/num_input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int num;
+	printf("enter a number\n");
+	scanf("%d", &num);
+	printf("You entered %d\n", num);
+	return 0;
+}
